8b4eaf560b396f4d933486540e597caa6bb37a28
AprDec <1263868407@qq.com>
2023-03-25 20:53:47 +0800

    自适应移动端

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings: ['在校大学生','Bilibili Up主','抖音爱好者'],
+    strings: ['在校大学生','Bilibili Up主','梦想成为海贼王的人'],
     typeSpeed:70,
     backSpeed:70,
     backDelay:1000,
